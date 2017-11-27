d2507b4a3200dce42b4ad261e029af430f0bf4e4
Noah Evans <clonex10100@gmail.com>
2017-11-27 14:32:49 -0600

    Created primezone

--- src/proteins/PrimeZone.java
+++ src/proteins/PrimeZone.java
@@ -1,7 +1,32 @@
 package proteins;
 public class PrimeZone{
+  /*
+  *Zone is in the form
+  *[[x,y],[helix,strand,nucleotide]]
+  */
   private int[][] zone;
-  PrimeZone(int type, int[] coords, int[] nucleotide){
-    zone = coords, nucleotide;
+  private Helix[] helix;
+  private static int RANGE 150;
+  public PrimeZone(int type, int[] coords, int[] nucleotide,Helix helix1,Helix helix2){
+    zone = new {coords, nucleotide};
+    helix = {helix1,helix2};
+  }
+  public int[] getCoords(){
+    return zone[0]
+  }
+  public void addNucleotide(Nucleotide nucleotide){
+    helix[zone[1][0]].addNucleotide(zone[1][1],zone[1][2],nucleotide);
+  }
+  public void toggleBond(){
+    helix[zone[1][0]].toggleNucleotide(zone[1][1],zone[1][2]);
+  }
+  public boolean inRange(int x, int y){
+    if((zone[1][0] - RANGE < x && x < zone[1][0] + RANGE) &&
+       (zone[0][1] - RANGE < y && y < zone[1][1] + RANGE)){
+       return true; 
+    }
+    else{
+      return falsez
+    }
   }
 }
