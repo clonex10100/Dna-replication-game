0ecf1483f01a468c94a0389cbfe5298a6c0c95ff
clonex10100 <clonex10100@gmail.com>
2017-11-29 22:21:40 -0600

    Fixed spacing

--- src/gameStages/MainGameLoop.java
+++ src/gameStages/MainGameLoop.java
@@ -34,72 +34,72 @@ public class MainGameLoop extends Application{
 		Machine machine = new Machine(originalHelix,unzipRange,root);
 		stage.setScene(scene);
 		stage.show();
-	    new AnimationTimer(){
-			int stage = 0;
-			long lastNano = 0;
-			int upperIndex = unzipRange;
-			int lowerIndex = 2;
-	        public void handle(long currentNanoTime){
-	        	if(currentNanoTime - lastNano > 900000) {
-    				gc.setFill(new Color(1,1,1, 1.0) );
-    				gc.fillRect(0,0, 1200,700);
-	        		if(stage == 0 || stage == 5) {
-	        			if(machine.isUnzipped()) {
-	        				stage+=1;
-	        			}
-	        		}
-	        		if(stage == 1) {
-	        			machine.addZone(0,0,0,upperIndex,50, 300);
-	        			machine.addZone(0,1,1,lowerIndex,50,400);
-	        			upperIndex--;
-	        			lowerIndex++;
-	        			stage++;
-	        		}
-	        		if(stage==2) {
-	        			if(machine.getPrimeZone(0).length==0) {
-	        				stage++;
-	        			}
-	        		}
-	        		if(stage==3) {
-	        			boolean empty = false;
-	        			for(int i =1; i<=4;i++) {
-	        				if(machine.getPrimeZone(i).length==0) {
-	        					empty=true;
-	        				}
-	        				else {
-	        					empty=false;
-	        					break;
-	        				}
-	        			}
-	        			if(empty) {
-									if(upperIndex==1){
-										stage++;
-									}
-									else{
-		        				machine.addComplementaryNucleotideZone(0,0,upperIndex,50,300);
-		        				machine.addComplementaryNucleotideZone(1,1,lowerIndex,50,400);
-		        				upperIndex--;
-		        				lowerIndex++;
-									}
-	        			}
-	        		}
-	        		if(stage==4) {
-	        			machine.setUnzipRange(10);
-								upperIndex = unzipRange+1;
-								lowerIndex = 2;
-	        			stage = 5;
-	        		}
-							if(stage ==6){
-								machine.addZone(0,1,1,lowerIndex,50,300);
-								machine.addComplementaryNucleotideZone(0,0,upperIndex,50,400);
-								upperIndex--;
-								lowerIndex++;
-								stage=2;
-							}
-	        		machine.draw(gc);
-	        		lastNano = currentNanoTime;
-	        	}
+	 	new AnimationTimer(){
+		int stage = 0;
+		long lastNano = 0;
+		int upperIndex = unzipRange;
+		int lowerIndex = 2;
+	  public void handle(long currentNanoTime){
+	  	if(currentNanoTime - lastNano > 900000) {
+    		gc.setFill(new Color(1,1,1, 1.0) );
+    		gc.fillRect(0,0, 1200,700);
+	    	if(stage == 0 || stage == 5) {
+	    		if(machine.isUnzipped()) {
+	     			stage+=1;
+	     		}
+	     	}
+	     	else if(stage == 1) {
+	     		machine.addZone(0,0,0,upperIndex,50, 300);
+	     		machine.addZone(0,1,1,lowerIndex,50,400);
+	     		upperIndex--;
+	     		lowerIndex++;
+	     		stage++;
+	     	}
+	     	else if(stage==2) {
+	     		if(machine.getPrimeZone(0).length==0) {
+	     			stage++;
+	     		}
+	     	}
+	     	else if(stage==3) {
+	     		boolean empty = false;
+	     		for(int i =1; i<=4;i++) {
+	     			if(machine.getPrimeZone(i).length==0) {
+	     				empty=true;
+	     			}
+	     			else {
+	     				empty=false;
+	     				break;
+	     			}
+	     		}
+	     		if(empty) {
+						if(upperIndex==1){
+							stage++;
+						}
+						else{
+		        	machine.addComplementaryNucleotideZone(0,0,upperIndex,50,300);
+		        	machine.addComplementaryNucleotideZone(1,1,lowerIndex,50,400);
+		        	upperIndex--;
+		        	lowerIndex++;
+						}
 	        }
-	    }.start();
+				}
+	      else if(stage==4) {
+	      	machine.setUnzipRange(10);
+					upperIndex = unzipRange+1;
+					lowerIndex = 2;
+	      	stage = 5;
+	      }
+				else if(stage ==6){
+					machine.addZone(0,1,1,lowerIndex,50,300);
+					machine.addComplementaryNucleotideZone(0,0,upperIndex,50,400);
+					upperIndex--;
+					lowerIndex++;
+					stage=2;
+				}
+	      machine.draw(gc);
+	      lastNano = currentNanoTime;
+	      }
+	    }
+		}.start();
 	}
 }
